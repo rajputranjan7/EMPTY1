dd50e4ecd41265c92c144be5d016cbe9c0196f59
sreehariperozhi <sreehariperozhi07@gmail.com>
2025-09-19 13:52:17 +0530

    Initial Commit

---
+++ EMPTY/EMPTYApp.swift
@@ -0,0 +1,17 @@
+//
+//  EMPTYApp.swift
+//  EMPTY
+//
+//  Created by student on 19/09/25.
+//
+
+import SwiftUI
+
+@main
+struct EMPTYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
