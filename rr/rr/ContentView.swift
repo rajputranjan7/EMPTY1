5734807bca5ed422d88581d8c59e9f3bf90018bc
rajputranjan7 <ranjan72525@gmail.com>
2025-12-04 15:31:20 +0530

    fully functional

--- rr/rr/ContentView.swift
+++ rr/rr/ContentView.swift
@@ -13,23 +13,42 @@ struct NotesApp: View {
     @Environment(\.modelContext)
     private var modelContext
     @Query private var Lists: [Listt]
+    
+    @State private var title: String = ""
+    @State private var isAlertShowing :Bool = false
     var body: some View {
         NavigationStack{
             List{
                     ForEach(Lists) {
                         Lists in
                     Text(Lists.title)
+                            .font(.title)
+                            .fontWeight(.light)
+                            .padding(.vertical,2)
                                }
             }
             .navigationTitle("My list")
             .toolbar{
                 ToolbarItem(placement:.topBarTrailing){
                     Button{
-                        
+                        isAlertShowing.toggle()
                     }label: {
                         Image(systemName: "plus")
+                            .imageScale(.large)
                     }
                 }
+               
+            }
+            .alert("create a new view", isPresented:$isAlertShowing){
+                TextField("Enter a list:",text:$title)
+                Button()
+                    {
+                        modelContext.insert(Listt(title:title))
+                        title = ""
+                    } label:{
+                        Text("save")
+                    }
+                    .disabled(title.isEmpty)
             }
             .overlay{
                 if Lists.isEmpty{
@@ -42,8 +61,22 @@ struct NotesApp: View {
         }
     }
 }
+#Preview("Second list"){
+    let container = try! ModelContainer(for: Listt.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+    
+    let ctx = container.mainContext
+        ctx.insert(Listt(title: "swift Coding club"))
+        ctx.insert(Listt(title: "swift Coding1 club"))
+        ctx.insert(Listt(title: "swift Coding2 club"))
 
-#Preview {
+
+
+    return NotesApp()
+        .modelContainer(container)
+}
+
+#Preview("Main list") {
     NotesApp()
+        .modelContainer(for: Listt.self,inMemory:true)
 }
 
