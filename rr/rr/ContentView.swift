406aedcf640371995ec4eb9d862a5b411dae3b04
rajputranjan7 <ranjan72525@gmail.com>
2025-12-04 11:44:49 +0530

    tudu apk

---
+++ rr/rr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  rr
+//
+//  Created by student on 04/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ rr/rr/rrApp.swift
@@ -0,0 +1,17 @@
+//
+//  rrApp.swift
+//  rr
+//
+//  Created by student on 04/12/25.
+//
+
+import SwiftUI
+
+@main
+struct rrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
